a1cd9394fd32c9223240c10ce3ffc44ae338e87e
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2022-06-02 00:38:47 +0200

    Level_1 w osobnym pliku

--- ProjektGra.py
+++ ProjektGra.py
@@ -1,6 +1,6 @@
 import pygame, os
 import game_module as gm
-import Player, Platform, Item, Level
+import Player, Platform, Item, Level, Level_1
 
 pygame.init()
 
@@ -13,31 +13,14 @@ clock = pygame.time.Clock()
 
 
 
-class Level_1(Level.Level):
-    def __init__(self, player):
-        super().__init__(player)
-        self._create_platforms()
-        self._create_items()
 
-
-    def _create_platforms(self):
-        platforms_cor = [[15*70, 70, 70, gm.HEIGHT - 70],
-                         [4*70, 70, 200, 370],
-                         [70, 70, 1000, 370]]
-
-        for cor in platforms_cor:
-            self.set_of_platforms.add(Platform.Platform(gm.GRASS_LIST, *cor))
-
-    def _create_items(self):
-        shotgun = Item.Item(gm.SHOTGUN2, 'shotgun', 700, 620)
-        self.set_of_items.add(shotgun)
 
 
 #konkretyzacja obiekt�w
 player = Player.Player(gm.KAPITAN_R)
 player.rect.left = 150
 player.rect.bottom = gm.HEIGHT - 70
-current_level = Level_1(player)
+current_level = Level_1.Level_1(player)
 player.level = current_level
 
 
