227994d40834b5f205d4efea3bf2f7d2a14a2c04
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2022-06-01 20:30:26 +0200

    grafika kapitana

--- GameModule.py
+++ GameModule.py
@@ -8,7 +8,6 @@ DARKGREEN = pygame.color.THECOLORS['darkgreen']
 LIGHTBLUE = pygame.color.THECOLORS['lightblue']
 
 
-
 screen = pygame.display.set_mode(SIZESCREEN)
 
 # grafika  - wczytywanie grafik
@@ -17,6 +16,7 @@ file_names = sorted(os.listdir(path))
 
 file_names.remove('background.png')
 BACKGROUND = pygame.image.load(os.path.join(path, 'background.png')).convert()
+
 for file_name in file_names:
     image_name = file_name[:-4].upper()
     globals()[image_name] = pygame.image.load(os.path.join(path, file_name)).convert_alpha(BACKGROUND)
