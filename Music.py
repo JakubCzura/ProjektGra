2240742e3d46e69facf4d6a9a43bb69f2df52e96
JakubCzura <76125047+JakubCzura@users.noreply.github.com>
2022-06-16 21:13:10 +0200

    ok

--- Music.py
+++ Music.py
@@ -7,8 +7,8 @@ class Music():
         mixer.init()
         self.sound = mixer.Sound('music/' + self.title)
 
-        #¿eby muzyka gra³a w nieskoñczonoœæ daje siê -1
-        #¿eby muzyka raz siê odtworzy³a daje siê 0
+        #zeby muzyka grala w nieskonczonosc daje sie isInfinityPlaying  -1
+        #zeby muzyka grala raz daje sie isInfinityPlaying  0
     def PlayMusic(self):
         mixer.music.load('music/' + self.title)
         mixer.music.play(self.isInfinityPlaying)
